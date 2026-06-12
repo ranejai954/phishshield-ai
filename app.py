63d2ac54ee3fe5c8b4fde422519a025674704ced
ranejai954 <ranejai954@gmail.com>
2026-06-12 20:48:16 +0530

    Initial project setup

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="PhishShield AI",
+    page_icon="🛡️"
+)
+
+st.title("🛡️ PhishShield AI")
+
+st.write("Intelligent Phishing Detection & Threat Analysis System")
