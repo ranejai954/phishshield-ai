95c55583a5a47cff0d17f8699f98abcbfcf00653
ranejai954 <ranejai954@gmail.com>
2026-06-13 15:22:09 +0530

    Added history page with MySQL integration

--- database/db.py
+++ database/db.py
@@ -1,12 +1,16 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",  # your actual password
-        database="phishshield"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
 
 
